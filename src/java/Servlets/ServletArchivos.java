510294ad898ae057d5cf9832b4ed55ffdde0cf16
Kevin Monzon <kevineloy11@gmail.com>
2017-10-25 14:57:46 -0300

    arreglos

--- src/java/Servlets/ServletArchivos.java
+++ src/java/Servlets/ServletArchivos.java
@@ -53,160 +53,164 @@ public class ServletArchivos extends HttpServlet {
         rutaConfWS = rutaConfWS.replace( "%20", " ");
         InputStream entrada = new FileInputStream(rutaConfWS);
         propiedades.load(entrada);// cargamos el archivo de propiedades
-		
-        URL url = new URL("http://"+ propiedades.getProperty("ipServidor") +":"+ propiedades.getProperty("puertoWSArch")+"/"+propiedades.getProperty("nombreWSArch"));
-        WSArchivosService wsarchs = new WSArchivosService();
-        WSArchivos wsarch = wsarchs.getWSArchivosPort();
-        
-        request.getSession().setAttribute("WSArchivos", wsarch);
-        
-        if (request.getParameter("cargarDatosPrueba") != null) {
-            wsarch.cargadeDatos();
-            request.getSession().removeAttribute("Usuario");
-            request.getSession().removeAttribute("Album");
-            request.getSession().removeAttribute("temasAReproducir");
-            
-            response.getWriter().write("se han cargado los datos de prueba");
-        }
-        
-        String tipoArchivo = request.getParameter("tipo");
-        
-        if(tipoArchivo != null){
-            if (tipoArchivo.equals("audio")) {
-                try {
-                    String ruta = request.getParameter("ruta");
-                    response.setContentType("audio/mpeg");
-                    response.addHeader("Content-Disposition", "attachment; filename=" + "NombreTema.mp3"); //indica que es un archivo para descargar
-                    
-                    byte[] audio = wsarch.cargarArchivo(ruta);
-                    System.out.println(audio.length);
-                    
-                    response.setContentType("audio/mpeg");
-                    response.setContentLength((int) audio.length);
-                    
-                    OutputStream out = response.getOutputStream();
-                    out.write(audio);
-                    out.close();
-                    
-                    /*
-                    BufferedInputStream buf = wsarch.cargarTema(ruta);
-                    
-                    OutputStream out = response.getOutputStream();
-                    int readBytes = 0;
-                    //read from the file; write to the ServletOutputStream
-                    while ((readBytes = buf.read()) != -1)
-                    out.write(readBytes);
-                    
-                    out.close();
-                    buf.close();
-                    */
-                } catch (IOException_Exception ex) {
-                    Logger.getLogger(ServletArchivos.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            } else {
-                try {
-                    // tipo == "imagen"
-                    String ruta = request.getParameter("ruta");
-                    
-                    byte[] img = wsarch.cargarArchivo(ruta);
-                    response.setContentType("image/jpg");
-                    response.setContentLength((int) img.length);
-                    OutputStream out = response.getOutputStream();
-                    //ImageIO.write(img, "png", out);
-                    out.write(img);
-                    out.close();
-                } catch (IOException_Exception ex) {
-                    Logger.getLogger(ServletArchivos.class.getName()).log(Level.SEVERE, null, ex);
-                }
+	
+        try{
+            URL url = new URL("http://"+ propiedades.getProperty("ipServidor") +":"+ propiedades.getProperty("puertoWSArch")+"/"+propiedades.getProperty("nombreWSArch"));
+            WSArchivosService wsarchs = new WSArchivosService();
+            WSArchivos wsarch = wsarchs.getWSArchivosPort();
+
+            request.getSession().setAttribute("WSArchivos", wsarch);
+
+            if (request.getParameter("cargarDatosPrueba") != null) {
+                wsarch.cargadeDatos();
+                request.getSession().removeAttribute("Usuario");
+                request.getSession().removeAttribute("Album");
+                request.getSession().removeAttribute("temasAReproducir");
+
+                response.getWriter().write("se han cargado los datos de prueba");
             }
-        }
-        
-        if(request.getParameter("reproducirAlbum")!=null){
-            String album = request.getParameter("reproducirAlbum");
-            String artista = request.getParameter("artista");
-            String temaSeleccionado = request.getParameter("tema");
-//            response.getWriter().write(temaSeleccionado);
-            List<DtTema> temas = wsarch.reproducirAlbum(artista, album).getTemas();
-            request.getSession().setAttribute("temasAReproducir", temas);
-            byte[] imagen = wsarch.getImagenAlbum(artista, album);
-            if (imagen!=null){
-                try {
-                    String path = this.getClass().getClassLoader().getResource("").getPath();
-                    path = path.replace("build/web/WEB-INF/classes/","temporales/");
-                    path = path + album + "REPRODUCTOR.jpg";
-                    path = path.replace( "%20", " ");
-                    File f = new File(path);
-                    org.apache.commons.io.FileUtils.writeByteArrayToFile(f, imagen);
-                    request.getSession().setAttribute("ImagenAlbumReproductor", path);
-                } catch (FileNotFoundException ex) {ex.getMessage();
-                } catch (IOException ex) {ex.getMessage();}
-            }
-            else
-                if (request.getSession().getAttribute("ImagenAlbumReproductor")!=null)
-                    request.getSession().removeAttribute("ImagenAlbumReproductor");
-                
-            //Si es el rquest que se envia al seleccionar un tema
-            if(temaSeleccionado != null){
-                //Setear ese atributo para que se repdoduzca por defecto el tema seleccionado
-                for (DtTema tema : temas) {
-                    if(tema.getNombre().equals(temaSeleccionado)){
-                        request.getSession().setAttribute("reproducirTema", tema);
-                        break;
+
+            String tipoArchivo = request.getParameter("tipo");
+
+            if(tipoArchivo != null){
+                if (tipoArchivo.equals("audio")) {
+                    try {
+                        String ruta = request.getParameter("ruta");
+                        response.setContentType("audio/mpeg");
+                        response.addHeader("Content-Disposition", "attachment; filename=" + "NombreTema.mp3"); //indica que es un archivo para descargar
+
+                        byte[] audio = wsarch.cargarArchivo(ruta);
+                        System.out.println(audio.length);
+
+                        response.setContentType("audio/mpeg");
+                        response.setContentLength((int) audio.length);
+
+                        OutputStream out = response.getOutputStream();
+                        out.write(audio);
+                        out.close();
+
+                        /*
+                        BufferedInputStream buf = wsarch.cargarTema(ruta);
+
+                        OutputStream out = response.getOutputStream();
+                        int readBytes = 0;
+                        //read from the file; write to the ServletOutputStream
+                        while ((readBytes = buf.read()) != -1)
+                        out.write(readBytes);
+
+                        out.close();
+                        buf.close();
+                        */
+                    } catch (IOException_Exception ex) {
+                        Logger.getLogger(ServletArchivos.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                } else {
+                    try {
+                        // tipo == "imagen"
+                        String ruta = request.getParameter("ruta");
+
+                        byte[] img = wsarch.cargarArchivo(ruta);
+                        response.setContentType("image/jpg");
+                        response.setContentLength((int) img.length);
+                        OutputStream out = response.getOutputStream();
+                        //ImageIO.write(img, "png", out);
+                        out.write(img);
+                        out.close();
+                    } catch (IOException_Exception ex) {
+                        Logger.getLogger(ServletArchivos.class.getName()).log(Level.SEVERE, null, ex);
                     }
                 }
-            }else{
-                //Sino, si hay temas para reproducir, setear ese atributo para que se repdoduzca el primero por defecto
-                if(temas.isEmpty() == false){
-                    request.getSession().setAttribute("reproducirTema", temas.get(0));
+            }
+
+            if(request.getParameter("reproducirAlbum")!=null){
+                String album = request.getParameter("reproducirAlbum");
+                String artista = request.getParameter("artista");
+                String temaSeleccionado = request.getParameter("tema");
+    //            response.getWriter().write(temaSeleccionado);
+                List<DtTema> temas = wsarch.reproducirAlbum(artista, album).getTemas();
+                request.getSession().setAttribute("temasAReproducir", temas);
+                byte[] imagen = wsarch.getImagenAlbum(artista, album);
+                if (imagen!=null){
+                    try {
+                        String path = this.getClass().getClassLoader().getResource("").getPath();
+                        path = path.replace("build/web/WEB-INF/classes/","temporales/");
+                        path = path + album + "REPRODUCTOR.jpg";
+                        path = path.replace( "%20", " ");
+                        File f = new File(path);
+                        org.apache.commons.io.FileUtils.writeByteArrayToFile(f, imagen);
+                        request.getSession().setAttribute("ImagenAlbumReproductor", path);
+                    } catch (FileNotFoundException ex) {ex.getMessage();
+                    } catch (IOException ex) {ex.getMessage();}
                 }
-            }
-        }
-        
-        if(request.getParameter("reproducirLista")!=null){
-            String lista = request.getParameter("reproducirLista");
-            String creador = request.getParameter("creador");
-            String genero = request.getParameter("genero");
-            String temaSeleccionado = request.getParameter("tema");
-            
-            List<DtTema> temas;
-            
-            //Si tiene creador es una lista particular, sino por defecto
-            if(creador != null){
-                temas = wsarch.reproducirListaP(creador, lista).getTemas();
-            }else{
-                temas = wsarch.reproducirListaPD(genero, lista).getTemas();
-            }
-            
-            DtLista dt = (DtLista) request.getSession().getAttribute("Lista");
-            if (dt.getRutaImagen()!=null){
-                request.getSession().setAttribute("ImagenAlbumReproductor", dt.getRutaImagen());    
-            }
-            else
-                if (request.getSession().getAttribute("ImagenAlbumReproductor")!=null)
-                    request.getSession().removeAttribute("ImagenAlbumReproductor");
-            
-            request.getSession().setAttribute("temasAReproducir", temas);
-            
-            //Si es el rquest que se envia al seleccionar un tema
-            if(temaSeleccionado != null){
-                //Setear ese atributo para que se repdoduzca por defecto el tema seleccionado
-                for (DtTema tema : temas) {
-                    if(tema.getNombre().equals(temaSeleccionado)){
-                        request.getSession().setAttribute("reproducirTema", tema);
-                        break;
+                else
+                    if (request.getSession().getAttribute("ImagenAlbumReproductor")!=null)
+                        request.getSession().removeAttribute("ImagenAlbumReproductor");
+
+                //Si es el rquest que se envia al seleccionar un tema
+                if(temaSeleccionado != null){
+                    //Setear ese atributo para que se repdoduzca por defecto el tema seleccionado
+                    for (DtTema tema : temas) {
+                        if(tema.getNombre().equals(temaSeleccionado)){
+                            request.getSession().setAttribute("reproducirTema", tema);
+                            break;
+                        }
+                    }
+                }else{
+                    //Sino, si hay temas para reproducir, setear ese atributo para que se repdoduzca el primero por defecto
+                    if(temas.isEmpty() == false){
+                        request.getSession().setAttribute("reproducirTema", temas.get(0));
                     }
                 }
-            }else{
-                //Sino, si hay temas para reproducir, setear ese atributo para que se repdoduzca el primero por defecto
-                if(temas.isEmpty() == false){
-                    request.getSession().setAttribute("reproducirTema", temas.get(0));
+            }
+
+            if(request.getParameter("reproducirLista")!=null){
+                String lista = request.getParameter("reproducirLista");
+                String creador = request.getParameter("creador");
+                String genero = request.getParameter("genero");
+                String temaSeleccionado = request.getParameter("tema");
+
+                List<DtTema> temas;
+
+                //Si tiene creador es una lista particular, sino por defecto
+                if(creador != null){
+                    temas = wsarch.reproducirListaP(creador, lista).getTemas();
+                }else{
+                    temas = wsarch.reproducirListaPD(genero, lista).getTemas();
+                }
+
+                DtLista dt = (DtLista) request.getSession().getAttribute("Lista");
+                if (dt.getRutaImagen()!=null){
+                    request.getSession().setAttribute("ImagenAlbumReproductor", dt.getRutaImagen());    
+                }
+                else
+                    if (request.getSession().getAttribute("ImagenAlbumReproductor")!=null)
+                        request.getSession().removeAttribute("ImagenAlbumReproductor");
+
+                request.getSession().setAttribute("temasAReproducir", temas);
+
+                //Si es el rquest que se envia al seleccionar un tema
+                if(temaSeleccionado != null){
+                    //Setear ese atributo para que se repdoduzca por defecto el tema seleccionado
+                    for (DtTema tema : temas) {
+                        if(tema.getNombre().equals(temaSeleccionado)){
+                            request.getSession().setAttribute("reproducirTema", tema);
+                            break;
+                        }
+                    }
+                }else{
+                    //Sino, si hay temas para reproducir, setear ese atributo para que se repdoduzca el primero por defecto
+                    if(temas.isEmpty() == false){
+                        request.getSession().setAttribute("reproducirTema", temas.get(0));
+                    }
                 }
             }
-        }
-        
-        if(request.getParameter("cerrarReproductor") != null){
-            request.getSession().removeAttribute("temasAReproducir");
-            request.getSession().removeAttribute("reproducirTema");
+
+            if(request.getParameter("cerrarReproductor") != null){
+                request.getSession().removeAttribute("temasAReproducir");
+                request.getSession().removeAttribute("reproducirTema");
+            }
+        }catch(Exception ex){
+            response.sendRedirect("/EspotifyWeb/Vistas/Error.html");
         }
     }
 
